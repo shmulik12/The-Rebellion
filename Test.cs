d0b87aa237882c8c583190466c5c8d85470e2d84
dennisp1234 <dennispiat050@gmail.com>
2024-08-28 18:48:37 +0300

    Update Test.cs

--- Test.cs
+++ Test.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+int x;
     // Start is called before the first frame update
     void Start()
     {
-        
+      x = 1;  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(x);
+        x++;
     }
 }
